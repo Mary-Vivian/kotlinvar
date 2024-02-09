992a18c2594adf31a61827ca9afbae373e0b64bc
Maryvivian-Muthoni <muthonimaryvivian@gmail.com>
2024-02-09 07:10:03 +0300

    variable commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+val student1Name= "Hamisi Amali"
+val student1Age = 23
+val student1Weight= 56.4F
+val studentNationality= "Kenyan"
+val student1Phone = "0756890746"
+
+println(student1Name)
+println(student1Age)
+println(student1Weight)
+println(studentNationality)
+println(student1Phone)
+
+}
